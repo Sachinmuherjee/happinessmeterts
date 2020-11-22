98a95a5c1f7fc36cac040202d35427d8f0243780
sachinmukherjee <sachinmukherjee7789@gmail.com>
2020-11-22 19:15:06 +0530

    first commit

---
+++ src/Containers/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Smiley from './smiley'
+import '../Assets/Styles/App.css';
+
+
+const App=()=> {
+  return (
+    <div className="container-fluid">
+      
+      <Smiley></Smiley>
+      </div>
+      
+    
+      );
+}
+
+export default App;
